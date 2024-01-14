6a787894812c8cefb664d913b38d8a02a579b19f
Raymond Chooi <raymond_chooi282@hotmail.com>
2024-01-13 21:24:39 -0500

    Add sepolia to connectkit

--- apps/web/src/utils/connectkit.tsx
+++ apps/web/src/utils/connectkit.tsx
@@ -1,12 +1,15 @@
 "use client";
 
 import { WagmiConfig, createConfig } from "wagmi";
+import { sepolia, mainnet, optimism, arbitrum } from "wagmi/chains";
 import { ConnectKitProvider, getDefaultConfig } from "connectkit";
+
+const chains = [sepolia, mainnet, optimism, arbitrum];
 
 const config = createConfig(
   getDefaultConfig({
     // Required API Keys
-    alchemyId: process.env.ALCHEMY_ID, // or infuraId
+    alchemyId: process.env.ALCHEMY_ID,
     walletConnectProjectId:
       process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID ?? "",
 
@@ -14,6 +17,7 @@ const config = createConfig(
     appName: "hom3page",
 
     // Optional
+    chains,
     appDescription: "Home of web3",
     appUrl: "https://www.hom3page.com", // your app's url
     appIcon: "https://www.hom3page.com/logo_plain.jpg", // your app's icon, no bigger than 1024x1024px (max. 1MB)
