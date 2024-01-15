2136b3f6d1607636b9a24c75eb37a7df5b404b2d
E-Labs-io <92272091+E-Labs-io@users.noreply.github.com>
2024-01-15 19:52:16 +0000

    moved errors and events to interface adding recieve functions building Recipt tracking

--- onChain/contracts/interfaces/IBlockShop.sol
+++ onChain/contracts/interfaces/IBlockShop.sol
@@ -1,16 +1,50 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-interface IBlockShop {
+interface IBlockShore {
+    //          CCIP Errors
+    // Custom errors to provide more descriptive revert messages.
+    error NotEnoughBalance(uint256 currentBalance, uint256 calculatedFees); // Used to make sure contract has enough balance.
+    error NotBlockSalesContract();
+    error MessageNotFromBLockSales(address contractTringToMessage_);
+    error MessageNotFromSalesChain(uint64 chainMessageOriginated);
+    //          CCIP Events
+    // Event emitted when a message is sent to another chain.
+    event MessageSent(
+        bytes32 indexed messageId, // The unique ID of the CCIP message.
+        uint64 indexed destinationChainSelector, // The chain selector of the destination chain.
+        address receiver, // The address of the receiver on the destination chain.
+        string text, // The text being sent.
+        address feeToken, // the token address used to pay CCIP fees.
+        uint256 fees // The fees paid for sending the CCIP message.
+    );
+
+    // Event emitted when a message is received from another chain.
+    event MessageReceived(
+        bytes32 indexed messageId, // The unique ID of the CCIP message.
+        uint64 indexed sourceChainSelector, // The chain selector of the source chain.
+        address sender, // The address of the sender from the source chain.
+        string text // The text that was received.
+    );
+
+    //          Sales Errors
     error OrderExceedsMaxAmount();
     error ToManyElementsInBuyArray();
     error OrderToLargeMax10();
-
+    //          Sales Events
     event SaleMade(
         address indexed buyer_,
         uint256 indexed amount_,
         uint64 indexed chainId_
     );
+
+    struct Sale {
+        address buyer_;
+        bool multiBuy_;
+        uint256[][] singleToken_;
+        bool saleComplete_;
+        bool saleFailed_;
+    }
 
     function buyBlock(uint256 tokenId) external;
 
--- onChain/contracts/shop/BlockStore.sol
+++ onChain/contracts/shop/BlockStore.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import {OnlyActive, Ownable} from "../security/onlyActive.sol";
-import {IBlockShop} from "../interfaces/IBlockShop.sol";
+import {IBlockShore} from "../interfaces/IBlockShop.sol";
 import {IGhoToken} from "../interfaces/IGhoToken.sol";
 import {IRouterClient} from "@chainlink/contracts-ccip/src/v0.8/ccip/interfaces/IRouterClient.sol";
 import {OwnerIsCreator} from "@chainlink/contracts-ccip/src/v0.8/shared/access/OwnerIsCreator.sol";
@@ -11,7 +11,7 @@ import {Client} from "@chainlink/contracts-ccip/src/v0.8/ccip/libraries/Client.s
 import {LinkTokenInterface} from "@chainlink/contracts/src/v0.8/shared/interfaces/LinkTokenInterface.sol";
 import {CCIPReceiver} from "@chainlink/contracts-ccip/src/v0.8/ccip/applications/CCIPReceiver.sol";
 
-contract BlockStore is ReentrancyGuard, OnlyActive, IBlockShop {
+contract BlockStore is ReentrancyGuard, OnlyActive, IBlockShore {
     //  Set token cost to 100 $GHO
     //  Var to track contract sales
     uint256 internal constant COST_PER_BLOCK = 100 * 10 ** 18;
@@ -20,31 +20,23 @@ contract BlockStore is ReentrancyGuard, OnlyActive, IBlockShop {
     IRouterClient private s_router;
     LinkTokenInterface private s_linkToken;
     IGhoToken public immutable GHO;
+
+
     //  BlockSales Contract
     //  Optimism Goerli
     uint64 constant SALES_CONTRACT_CHAIN = 2664363617261496610;
     address immutable SALES_CONTRACT_ADDRESS;
 
-    //          CCIP DEMO FUNCTIONS
-    // Custom errors to provide more descriptive revert messages.
-    error NotEnoughBalance(uint256 currentBalance, uint256 calculatedFees); // Used to make sure contract has enough balance.
-    // Event emitted when a message is sent to another chain.
-    event MessageSent(
-        bytes32 indexed messageId, // The unique ID of the CCIP message.
-        uint64 indexed destinationChainSelector, // The chain selector of the destination chain.
-        address receiver, // The address of the receiver on the destination chain.
-        string text, // The text being sent.
-        address feeToken, // the token address used to pay CCIP fees.
-        uint256 fees // The fees paid for sending the CCIP message.
-    );
+    mapping(uint256 => Sale) _saleRecipes;
 
-    // Event emitted when a message is received from another chain.
-    event MessageReceived(
-        bytes32 indexed messageId, // The unique ID of the CCIP message.
-        uint64 indexed sourceChainSelector, // The chain selector of the source chain.
-        address sender, // The address of the sender from the source chain.
-        string text // The text that was received.
-    );
+
+    modifier onlySalesContract(address sender_, uint64 chain_) {
+        if (sender_ != SALES_CONTRACT_ADDRESS)
+            revert MessageNotFromBLockSales(sender_);
+        if (chain_ != SALES_CONTRACT_CHAIN) revert();
+        MessageNotFromSalesChain(chain_);
+        _;
+    }
 
     constructor(
         address router_,
@@ -92,57 +84,83 @@ contract BlockStore is ReentrancyGuard, OnlyActive, IBlockShop {
         if (succsess) {}
     }
 
+    function _startCrossChainPurchase(
+        uint256[][] tokenIds_,
+        address buyer_
+    ) internal {
+        bytes32 messageId = _sendMessage(
+            SALES_CONTRACT_CHAIN,
+            SALES_CONTRACT_ADDRESS,
+            text
+        );
+    }
+
     /// @notice Sends data to receiver on the destination chain.
-    /// @dev Assumes your contract has sufficient LINK.
-    /// @param destinationChainSelector The identifier (aka selector) for the destination blockchain.
-    /// @param receiver The address of the recipient on the destination blockchain.
-    /// @param text The string text to be sent.
+    /// @dev Assumes your contract has sufficient Native Token.
+    /// @param destinationChainSelector_ The identifier (aka selector) for the destination blockchain.
+    /// @param receiver_ The address of the recipient on the destination blockchain.
+    /// @param payload_ The string text to be sent.
     /// @return messageId The ID of the message that was sent.
     function _sendMessage(
-        uint64 destinationChainSelector,
-        address receiver,
-        string calldata text
+        uint64 destinationChainSelector_,
+        address receiver_,
+        string calldata payload_
     ) internal onlyOwner returns (bytes32 messageId) {
         // Create an EVM2AnyMessage struct in memory with necessary information for sending a cross-chain message
-        Client.EVM2AnyMessage memory evm2AnyMessage = Client.EVM2AnyMessage({
-            receiver: abi.encode(receiver), // ABI-encoded receiver address
-            data: abi.encode(text), // ABI-encoded string
-            tokenAmounts: new Client.EVMTokenAmount[](0), // Empty array indicating no tokens are being sent
-            extraArgs: Client._argsToBytes(
-                // Additional arguments, setting gas limit
-                Client.EVMExtraArgsV1({gasLimit: 200_000})
-            ),
-            // Set the feeToken  address, indicating LINK will be used for fees
-            feeToken: address(s_linkToken)
-        });
-
-        // Get the fee required to send the message
-        uint256 fees = s_router.getFee(
-            destinationChainSelector,
-            evm2AnyMessage
+        Client.EVM2AnyMessage memory evm2AnyMessage = _buildCCIPMessage(
+            receiver_,
+            payload_,
+            address(0)
         );
 
-        if (fees > s_linkToken.balanceOf(address(this)))
-            revert NotEnoughBalance(s_linkToken.balanceOf(address(this)), fees);
+        // Initialize a router client instance to interact with cross-chain router
+        IRouterClient router = IRouterClient(this.getRouter());
 
-        // approve the Router to transfer LINK tokens on contract's behalf. It will spend the fees in LINK
-        s_linkToken.approve(address(s_router), fees);
+        // Get the fee required to send the CCIP message
+        uint256 fees = router.getFee(destinationChainSelector_, evm2AnyMessage);
 
-        // Send the message through the router and store the returned message ID
-        messageId = s_router.ccipSend(destinationChainSelector, evm2AnyMessage);
+        if (fees > address(this).balance)
+            revert NotEnoughBalance(address(this).balance, fees);
+
+        // Send the CCIP message through the router and store the returned CCIP message ID
+        messageId = router.ccipSend{value: fees}(
+            destinationChainSelector_,
+            evm2AnyMessage
+        );
 
         // Emit an event with message details
         emit MessageSent(
             messageId,
-            destinationChainSelector,
-            receiver,
-            text,
-            address(s_linkToken),
+            destinationChainSelector_,
+            receiver_,
+            payload_,
+            address(0),
             fees
         );
 
-        // Return the message ID
+        // Return the CCIP message ID
         return messageId;
+    }
+
+    /// handle a received message
+    function _ccipReceive(
+        Client.Any2EVMMessage memory any2EvmMessage
+    )
+        internal
+        onlySalesContract(
+            any2EvmMessage.sourceChainSelector,
+            abi.decode(any2EvmMessage.sender, (address))
+        )
+    {
+        s_lastReceivedMessageId = any2EvmMessage.messageId; // fetch the messageId
+        s_lastReceivedText = abi.decode(any2EvmMessage.data, (string)); // abi-decoding of the sent text
+
+        emit MessageReceived(
+            any2EvmMessage.messageId,
+            any2EvmMessage.sourceChainSelector, // fetch the source chain identifier (aka selector)
+            abi.decode(any2EvmMessage.sender, (address)), // abi-decoding of the sender address,
+            abi.decode(any2EvmMessage.data, (string))
+        );
     }
 
     function getBlockCost() public pure returns (uint256) {
