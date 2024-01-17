558b0b041c7dbe2b96a56bf8dadd2593340469a1
Raymond Chooi <raymond_chooi282@hotmail.com>
2024-01-17 12:51:57 -0500

    Add radial purple bg

--- apps/web/src/app/[id]/page.tsx
+++ apps/web/src/app/[id]/page.tsx
@@ -2,7 +2,7 @@ import { Wall } from "components";
 
 export default function WallPage() {
   return (
-    <main className="absolute left-0 top-0 flex h-screen w-screen bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+    <main className="absolute left-0 top-0 flex h-screen w-screen bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-violet-900 via-violet-950 to-gray-950 text-white">
       <h1 className="sr-only">Hom3page</h1>
       <div className="mt-[52px] h-[calc(100vh-52px)] w-screen ">
         <Wall />
