24ba8c9bfaddc8eb5e068a935bf6f9e8f709d2dd
E-Labs-io <92272091+E-Labs-io@users.noreply.github.com>
2024-01-18 23:12:49 +0000

    added Contracts object for stable deployments

---
+++ apps/web/src/constants/ABIs/contracts.ts
@@ -0,0 +1,65 @@
+
+import BlockSalesABI from "./blockSales.abi.json"
+import BlockStoreABI from "./blockSales.abi.json"
+import BlockTokenABI from "./blockSales.abi.json"
+import HomeProfileABI from "./blockSales.abi.json"
+
+
+export type ChainName =
+  | "eth"
+  | "ethGoerli"
+  | "ethSepolia"
+  | "arbitrum"
+  | "arbGoerli"
+  | "arbSepolia"
+  | "optimism"
+  | "opGoerli"
+  | "matic"
+  | "maticMumbai"
+  | "baseGoerli"
+  | "hardhat"
+  | "localhost";
+
+
+export type ContractNames =
+  | "BlockToken"
+  | "BlockSales"
+  | "BlockStore"
+  | "Hom3ProfileToken"
+  | "Hom3ProfileShadow"
+  | "Hom3Vault"
+  | "Hom3DepositVault";
+
+  export type ContractStore = {[contract in ContractNames]?:  {address: string , abi: any}}
+  export type NetworkContracts = {[ChainName in string]: ContractStore};
+
+const Contracts: NetworkContracts = {
+    ethSepolia:  {
+        BlockStore: {
+                address: "0x03F09634Ca9Cd65999e4Dbd15bc558F757e67B00", abi:BlockStoreABI 
+        }
+    },
+  
+    maticMumbai:  {
+        BlockSales: {
+                address: "0xd569f2D569C7439c869208B533436c1F6474c397", abi:BlockSalesABI 
+        },
+         BlockToken: {
+                address: "0x08a41fe84A22bf962559886A0c0957fc7E84af4D", abi:BlockTokenABI 
+        },  
+        Hom3ProfileToken: {
+                address: "", abi:HomeProfileABI 
+        }
+    }
+    
+}
+    export default Contracts;
+
+
+
+/**  {BlockToken: {},
+    BlockSales: {},
+    BlockStore: {},
+    ProfileToken: {},
+    Hom3Vault: {},
+    Hom3ProfileToken:{} */
--- onChain/bin/deployedAddress.ts
+++ onChain/bin/deployedAddress.ts
@@ -10,24 +10,24 @@ const deployedContracts: {
     BlockToken: "",
     BlockSales: "",
     BlockStore: "",
-    ProfileToken: "",
+    Hom3ProfileToken: "",
   },
   ethSepolia: {
     BlockToken: "",
     BlockStore: "0x03F09634Ca9Cd65999e4Dbd15bc558F757e67B00",
-    ProfileToken: "",
+    Hom3ProfileToken: "",
     Hom3DepositVault: "",
   },
   opGoerli: {
     BlockToken: "",
     BlockSales: "",
-    ProfileToken: "",
+    Hom3ProfileToken: "",
   },
   maticMumbai: {
     BlockToken: "0x08a41fe84A22bf962559886A0c0957fc7E84af4D",
     BlockSales: "0xd569f2D569C7439c869208B533436c1F6474c397",
     BlockStore: "",
-    ProfileToken: "",
+    Hom3ProfileToken: "",
     Hom3Vault: "",
   },
 };
@@ -36,7 +36,8 @@ export type ContractNames =
   | "BlockToken"
   | "BlockSales"
   | "BlockStore"
-  | "ProfileToken"
+  | "Hom3ProfileToken"
+  | "Hom3ProfileShadow"
   | "Hom3Vault"
   | "Hom3DepositVault";
 export default deployedContracts;
